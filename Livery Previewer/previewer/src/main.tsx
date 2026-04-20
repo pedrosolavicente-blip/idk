3ac64e0e30a4393bc8c89d279440f65e4691841f
itzz_link <pedrosolavicente@gmail.com>
2026-04-20 12:52:17 +0200

    Update main.tsx

--- Livery Previewer/previewer/src/main.tsx
+++ Livery Previewer/previewer/src/main.tsx
@@ -1,17 +1,15 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import App from './app/App.tsx'
-import LandingPage from './app/components/LandingPage.tsx'
 import './styles/index.css'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <BrowserRouter>
       <Routes>
-        <Route path="/"          element={<LandingPage />} />
         <Route path="/previewer" element={<App />} />
-        <Route path="*"          element={<Navigate to="/" replace />} />
+        <Route path="*"          element={<App />} />
       </Routes>
     </BrowserRouter>
   </StrictMode>,
